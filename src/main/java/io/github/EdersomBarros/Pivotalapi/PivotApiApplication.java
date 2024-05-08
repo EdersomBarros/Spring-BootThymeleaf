b066c65ed1291c9070f3335b6c73527ea7b61992
Ederson Apa Barros Rodrigues <ederson.rodrigues@PMCG.IMTI>
2024-05-08 10:36:03 -0400

    first commit

---
+++ src/main/java/io/github/EdersomBarros/Pivotalapi/PivotApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.EdersomBarros.Pivotalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PivotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/EdersomBarros/Pivotalapi/PivotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.EdersomBarros.Pivotalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
