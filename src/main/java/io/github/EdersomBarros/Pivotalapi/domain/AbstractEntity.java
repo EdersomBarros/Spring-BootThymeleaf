60d0d2efdc90dd15cc17bab7f93e9f97dcddd3b3
ederson barros <edersombarros@hotmail.com>
2024-05-08 17:02:31 -0400

    incluindo o entidades

--- src/main/java/io/github/EdersomBarros/Pivotalapi/domain/AbstractEntity.java
+++ src/main/java/io/github/EdersomBarros/Pivotalapi/domain/AbstractEntity.java
@@ -1,4 +1,36 @@
 package io.github.EdersomBarros.Pivotalapi.domain;
 
-public class AbstractEntity {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Objects;
+@MappedSuperclass
+@Data
+public abstract class AbstractEntity <ID extends Serializable> implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity<?> that = (AbstractEntity<?>) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id;
+    }
 }
